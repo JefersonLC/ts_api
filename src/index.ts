4bfa786cc79867a1637cebdd9f2a080cdf93712a
JefersonLC <c.jeferson.lc@gmail.com>
2023-01-04 00:06:33 -0500

    updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { ReqBody } from './interfaces/iIndex';
 
 const app = express();
 
@@ -8,8 +9,15 @@ app.listen(PORT, () => {
   console.log(`Server on port ${PORT}`);
 });
 
-app.get('/', (_req, res) => {
-  res.json({
+app.use(express.json());
+
+app.get('/', (_req: Request, res: Response) => {
+  res.status(200).json({
     hola: 'mundo',
   });
+});
+
+app.post('/', (req: Request, res: Response) => {
+  const body: ReqBody = req.body;
+  res.json(body);
 });
