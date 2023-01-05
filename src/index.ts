1bee405fd3f3022787e4b7c51d2b52971c34a8f1
JefersonLC <c.jeferson.lc@gmail.com>
2023-01-04 21:54:55 -0500

    updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+import 'reflect-metadata';
 import express, { Request, Response } from 'express';
-import { ReqBody } from './interfaces/iIndex';
+// import { ReqBody } from './interfaces/iIndex';
+import { AppDataSource } from './db';
+import { User } from './db/entities/User';
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) =>
+    console.error('Error during Data Source initialization:', err)
+  );
 
 app.listen(PORT, () => {
   console.log(`Server on port ${PORT}`);
@@ -11,13 +22,14 @@ app.listen(PORT, () => {
 
 app.use(express.json());
 
-app.get('/', (_req: Request, res: Response) => {
-  res.status(200).json({
-    hola: 'mundo',
-  });
+app.get('/', async (_req: Request, res: Response) => {
+  const users = await AppDataSource.getRepository(User).find()
+  res.status(200).json(users);
 });
 
-app.post('/', (req: Request, res: Response) => {
-  const body: ReqBody = req.body;
-  res.json(body);
+app.post('/', async (req: Request, res: Response) => {
+  const body = req.body;
+  const user = AppDataSource.getRepository(User).create(body)
+  const result = await AppDataSource.getRepository(User).save(user)
+  res.json(result)
 });
