bef3249969798175b3f7450b78348b989977be58
JefersonLC <c.jeferson.lc@gmail.com>
2023-01-06 00:41:35 -0500

    updated userSchema.ts

--- src/db/schemas/userSchema.ts
+++ src/db/schemas/userSchema.ts
@@ -1,23 +1,35 @@
 import Joi, { StringSchema, AnySchema } from 'joi';
 
-const name: StringSchema<string> = Joi.string().min(2).trim().messages({
+const id: StringSchema<string> = Joi.string().min(10).trim();
+const name: StringSchema<string> = Joi.string().min(2).max(20).trim().messages({
   'string.base': 'Name must be text',
   'string.empty': 'Name must not be empty',
   'string.min': 'Name must have at least 2 characters',
+  'string.max': 'Very long name',
   'any.required': 'Name is required',
 });
-const lastname: StringSchema<string> = Joi.string().min(2).trim().messages({
-  'string.base': 'Lastname must be text',
-  'string.empty': 'Lastname must not be empty',
-  'string.min': 'Lastname must have at least 2 characters',
-  'any.required': 'Lastname is required',
-});
-const email: StringSchema<string> = Joi.string().email().trim().messages({
-  'string.base': 'Email must be text',
-  'string.empty': 'Email must not be empty',
-  'string.email': 'Email is invalid',
-  'any.required': 'Email is required',
-});
+const lastname: StringSchema<string> = Joi.string()
+  .min(2)
+  .max(20)
+  .trim()
+  .messages({
+    'string.base': 'Lastname must be text',
+    'string.empty': 'Lastname must not be empty',
+    'string.min': 'Lastname must have at least 2 characters',
+    'string.max': 'Very long lastname',
+    'any.required': 'Lastname is required',
+  });
+const email: StringSchema<string> = Joi.string()
+  .email()
+  .max(50)
+  .trim()
+  .messages({
+    'string.base': 'Email must be text',
+    'string.empty': 'Email must not be empty',
+    'string.email': 'Email is invalid',
+    'string.max': 'Very long email',
+    'any.required': 'Email is required',
+  });
 const password: StringSchema<string> = Joi.string().min(8).trim().messages({
   'string.base': 'Password must be text',
   'string.empty': 'Password must not be empty',
@@ -31,10 +43,14 @@ const repeat_password: AnySchema<any> = Joi.equal(Joi.ref('password')).messages(
   }
 );
 
-export const createUser: Joi.ObjectSchema<any> = Joi.object({
+export const createUserSchema: Joi.ObjectSchema<any> = Joi.object({
   name: name.required(),
   lastname: lastname.required(),
   email: email.required(),
   password: password.required(),
   repeat_password: repeat_password.required(),
+});
+
+export const getUserSchema: Joi.ObjectSchema<any> = Joi.object({
+  id: id.required(),
 });
