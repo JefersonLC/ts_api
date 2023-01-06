0851f9d40d0be30de1b8432e0a2f1d8da964ebe0
JefersonLC <c.jeferson.lc@gmail.com>
2023-01-06 00:40:08 -0500

    updated UserService.ts

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -10,6 +10,49 @@ import { NewUser } from '../types/user';
 const uid: ShortUniqueId = new ShortUniqueId({ length: 10 });
 
 export default class UserService {
+  async find(): Promise<User[]> {
+    const users: User[] = await AppDataSource.getRepository(User).find({
+      select: {
+        id: true,
+        name: true,
+        lastname: true,
+        email: true,
+        isAdmin: true,
+        verified: true,
+      },
+    });
+    return users;
+  }
+
+  async findById(id: string): Promise<User[]> {
+    const user: User[] = await AppDataSource.getRepository(User).find({
+      select: {
+        id: true,
+        name: true,
+        lastname: true,
+        email: true,
+        isAdmin: true,
+        verified: true,
+      },
+      where: {
+        id: id,
+      },
+    });
+    if (!user) {
+      throw boom.notFound('User not found');
+    }
+    return user;
+  }
+
+  async isDuplicated({ email }: NewUser): Promise<User | null> {
+    const user: User | null = await AppDataSource.getRepository(User).findOneBy(
+      {
+        email: email,
+      }
+    );
+    return user;
+  }
+
   async create(data: NewUser): Promise<User> {
     const isDuplicated: User | null = await this.isDuplicated(data);
     if (isDuplicated) {
@@ -35,12 +78,20 @@ export default class UserService {
     return result;
   }
 
-  async isDuplicated({ email }: NewUser) {
-    const user: User | null = await AppDataSource.getRepository(User).findOneBy(
-      {
-        email: email,
-      }
-    );
-    return user;
-  }
+  // async findByRole(value: boolean) {
+  //   const users: User[] = await AppDataSource.getRepository(User).find({
+  //     select: {
+  //       id: true,
+  //       name: true,
+  //       lastname: true,
+  //       email: true,
+  //       isAdmin: true,
+  //       verified: true,
+  //     },
+  //     where: {
+  //       isAdmin: value,
+  //     },
+  //   });
+  //   return users;
+  // }
 }
