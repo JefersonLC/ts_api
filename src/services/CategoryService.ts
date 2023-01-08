8cf7ea62c964acdd83892e8f09801a274417b165
JefersonLC <c.jeferson.lc@gmail.com>
2023-01-07 23:09:44 -0500

    added CategoryService.ts and ProductService.ts

---
+++ src/services/CategoryService.ts
@@ -0,0 +1,74 @@
+import boom from '@hapi/boom';
+import ShortUniqueId from 'short-unique-id';
+import { UpdateResult } from 'typeorm';
+import { AppDataSource } from '../db';
+import { Category } from '../db/entities/Category';
+import { NewCategory, UpdateCategory } from '../types/category';
+
+const uid: ShortUniqueId = new ShortUniqueId({ length: 10 });
+
+export default class CategoryService {
+  async findAll(): Promise<Category[]> {
+    const categories: Category[] = await AppDataSource.getRepository(
+      Category
+    ).find({
+      relations: {
+        products: true,
+      },
+    });
+    return categories;
+  }
+
+  async findById(id: string): Promise<Category> {
+    const [category]: Category[] = await AppDataSource.getRepository(
+      Category
+    ).findBy({
+      id: id,
+    });
+    if (!category) {
+      throw boom.notFound('Category not found');
+    }
+    return category;
+  }
+
+  async isDuplicated({ name }: NewCategory): Promise<Category | null> {
+    const category: Category | null = await AppDataSource.getRepository(
+      Category
+    ).findOneBy({
+      name: name,
+    });
+    return category;
+  }
+
+  async createCategory(data: NewCategory): Promise<Category> {
+    const isDuplicated: Category | null = await this.isDuplicated(data);
+    if (isDuplicated) {
+      throw boom.conflict('The category is already registered');
+    }
+    const category = AppDataSource.getRepository(Category).create({
+      id: uid(),
+      name: data.name,
+    });
+    const result = await AppDataSource.getRepository(Category).save(category);
+    return result;
+  }
+
+  async updateCategory(
+    data: UpdateCategory,
+    id: string
+  ): Promise<UpdateResult> {
+    const category = await this.findById(id);
+    const result: UpdateResult = await AppDataSource.getRepository(
+      Category
+    ).update(category.id, data);
+    return result;
+  }
+
+  async deleteCategory(id: string) {
+    const category = await this.findById(id);
+    const result: Category = await AppDataSource.getRepository(Category).remove(
+      category
+    );
+    return result;
+  }
+}
---
+++ src/services/ProductService.ts
@@ -0,0 +1,80 @@
+import boom from '@hapi/boom';
+import ShortUniqueId from 'short-unique-id';
+import { AppDataSource } from '../db';
+import { Product } from '../db/entities/Product';
+import { NewProduct } from '../types/product';
+
+const uid: ShortUniqueId = new ShortUniqueId({ length: 10 });
+
+export default class ProductService {
+  async findAll(): Promise<Product[]> {
+    const products: Product[] = await AppDataSource.getRepository(Product).find(
+      {
+        relations: {
+          category: true,
+        },
+      }
+    );
+    return products;
+  }
+
+  async findById(id: string): Promise<Product | null> {
+    const product: Product | null = await AppDataSource.getRepository(
+      Product
+    ).findOne({
+      where: {
+        id: id,
+      },
+      relations: {
+        category: true,
+      },
+    });
+    if (!product) {
+      throw boom.notFound('Product not found');
+    }
+    return product;
+  }
+
+  async isDuplicated({ name }: NewProduct): Promise<Product | null> {
+    const product: Product | null = await AppDataSource.getRepository(
+      Product
+    ).findOneBy({
+      name: name,
+    });
+    return product;
+  }
+
+  async createProduct(data: NewProduct): Promise<Product> {
+    const isDuplicated: Product | null = await this.isDuplicated(data);
+    if (isDuplicated) {
+      throw boom.conflict('The product is already registered');
+    }
+    const product = AppDataSource.getRepository(Product).create({
+      id: uid(),
+      name: data.name,
+      description: data.description,
+      price: data.price,
+      stock: data.stock,
+      photo: data.photo,
+      category: data.category,
+    });
+    const result = await AppDataSource.getRepository(Product).save(product);
+    return result;
+  }
+
+  // async updateProduct(data: UpdateProduct, id: string): Promise<UpdateResult> {
+  //   const Product = await this.findById(id);
+  //   const result: UpdateResult = await AppDataSource.getRepository(
+  //     Product
+  //   ).update(Product.id, data);
+  //   return result;
+  // }
+
+  // async deleteProduct(id: string) {
+  //   const Product = await this.findById(id);
+  //   const result: Product = await AppDataSource.getRepository(Product).remove(
+  //     Product
+  //   );
+  //   return result;
+  // }
+}
