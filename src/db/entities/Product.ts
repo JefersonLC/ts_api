154cb36a8050243c7df4cb7f1f2012a9de888d41
JefersonLC <c.jeferson.lc@gmail.com>
2023-01-07 23:02:55 -0500

    added Product.ts

---
+++ src/db/entities/Product.ts
@@ -0,0 +1,54 @@
+import { Entity, Column, PrimaryColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { Category } from './Category';
+
+@Entity()
+export class Product {
+  @PrimaryColumn({
+    type: 'varchar',
+    length: '10',
+    nullable: false,
+  })
+  id: string;
+
+  @Column({
+    type: 'varchar',
+    length: '20',
+    unique: true,
+    nullable: false,
+  })
+  name: string;
+
+  @Column({
+    type: 'varchar',
+    length: '200',
+    nullable: false,
+  })
+  description: string;
+
+  @Column({
+    type: 'double precision',
+    nullable: false,
+  })
+  price: number;
+
+  @Column({
+    type: 'int',
+    nullable: false,
+  })
+  stock: number;
+
+  @Column({
+    type: 'varchar',
+    length: '150',
+    nullable: true,
+  })
+  photo: string;
+
+  @ManyToOne(() => Category, (category) => category.products, {
+    nullable: false,
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'categoryId' })
+  category: Category;
+}
