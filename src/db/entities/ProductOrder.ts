3c7767ca7c023413a03aa7d15d71ae61ae736e95
JefersonLC <c.jeferson.lc@gmail.com>
2023-01-08 01:52:33 -0500

    added ProductOrder.ts

---
+++ src/db/entities/ProductOrder.ts
@@ -0,0 +1,49 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import { Order } from './Order';
+import { Product } from './Product';
+
+@Entity()
+export class ProductOrder {
+  @PrimaryColumn({
+    type: 'varchar',
+    length: '10',
+    nullable: false,
+  })
+  id: string;
+
+  @Column({
+    type: 'int',
+    nullable: false,
+  })
+  amount: number;
+
+  @Column({
+    type: 'double precision',
+    nullable: false,
+    name: 'unit_price',
+  })
+  unitPrice: number;
+
+  @Column({
+    type: 'double precision',
+    nullable: false,
+    name: 'total_price',
+  })
+  totalPrice: number;
+
+  @ManyToOne(() => Order, (order) => order.detail, {
+    nullable: false,
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'orderId' })
+  order: Order;
+
+  @ManyToOne(() => Product, (product) => product.detail, {
+    nullable: false,
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'productId' })
+  product: Product;
+}
