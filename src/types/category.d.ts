be29fa02d16f28d20598c15ddc162a6ce443e5ec
JefersonLC <c.jeferson.lc@gmail.com>
2023-01-07 23:10:11 -0500

    added category.d.ts and product.d.ts

---
+++ src/types/category.d.ts
@@ -0,0 +1,5 @@
+export type NewCategory = {
+  name: string;
+};
+
+export type UpdateCategory = NewCategory;
---
+++ src/types/product.d.ts
@@ -0,0 +1,10 @@
+import { Category } from "../db/entities/Category";
+
+export type NewProduct = {
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  photo: string;
+  category: Category;
+};
