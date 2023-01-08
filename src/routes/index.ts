11b0fe5a6888efaaf239ef3399abe46c85af07bb
JefersonLC <c.jeferson.lc@gmail.com>
2023-01-07 23:08:06 -0500

    modified index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,10 @@
 import { Router } from 'express';
 import { userRouter } from './userRouter';
+import { categoryRouter } from './categoryRouter';
+import { productRouter } from './productRouter';
 
 export const apiRouter: Router = Router();
 
 apiRouter.use('/users', userRouter);
+apiRouter.use('/categories', categoryRouter);
+apiRouter.use('/products', productRouter);
