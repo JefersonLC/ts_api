e1216942e1db49d9fa0fbf4ab0985b00e572c67c
JefersonLC <c.jeferson.lc@gmail.com>
2023-01-07 23:02:23 -0500

    added Category.ts

---
+++ src/db/entities/Category.ts
@@ -0,0 +1,23 @@
+import { Entity, Column, PrimaryColumn, OneToMany } from 'typeorm';
+import { Product } from './Product';
+
+@Entity()
+export class Category {
+  @PrimaryColumn({
+    type: 'varchar',
+    length: '10',
+    nullable: false,
+  })
+  id: string;
+
+  @Column({
+    type: 'varchar',
+    length: '20',
+    unique: true,
+    nullable: false,
+  })
+  name: string;
+
+  @OneToMany(() => Product, (product) => product.category)
+  products: Product[];
+}
