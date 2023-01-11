b0c21ef4137a7522fc2c17d6419f1b3d714fe9db
JefersonLC <c.jeferson.lc@gmail.com>
2023-01-10 20:32:25 -0500

    add auth.js

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,11 @@
+import boom from '@hapi/boom';
+import { Response, NextFunction } from 'express';
+import { User } from '../db/entities/User';
+
+export function checkRole(req: any, _res: Response, next: NextFunction) {
+  const user: User = req.user;
+  if (!user.isAdmin) {
+    next(boom.unauthorized());
+  }
+  next();
+}
