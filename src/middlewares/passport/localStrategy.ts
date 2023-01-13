2196496547beb600e0fdbf8bcc4a0f1ed235bd42
JefersonLC <c.jeferson.lc@gmail.com>
2023-01-12 20:15:04 -0500

    updated localStrategy.ts

--- src/middlewares/passport/localStrategy.ts
+++ src/middlewares/passport/localStrategy.ts
@@ -1,3 +1,4 @@
+import boom from '@hapi/boom';
 import { Strategy } from 'passport-local';
 import bcrypt from 'bcrypt';
 import UserService from '../../services/UserService';
@@ -13,15 +14,14 @@ export const localStrategy = new Strategy(
   async (email: string, password: string, done): Promise<void> => {
     try {
       const user: User | null = await userService.findByEmail(email);
-      if (
-        !user ||
-        !bcrypt.compareSync(password, user.password) ||
-        !user.verified
-      )
-        return done(null, false);
+      if (!user) return done(boom.notFound('User not found'), false);
+      if (!bcrypt.compareSync(password, user.password))
+        return done(boom.badData('Incorrect password'), false);
+      if (!user.verified)
+        return done(boom.unauthorized('Email has not been verified'), false);
       else return done(null, user);
     } catch (error) {
-      return done(error);
+      return done(boom.unauthorized());
     }
   }
 );
