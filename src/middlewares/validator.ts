1283451419d05e9bdaf0643ee160c3f116c4b79a
JefersonLC <c.jeferson.lc@gmail.com>
2023-01-22 21:58:08 -0500

    updated validator.ts

--- src/middlewares/validator.ts
+++ src/middlewares/validator.ts
@@ -1,26 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 import Joi from 'joi';
 import boom from '@hapi/boom';
+import { request } from '../types/requestEnum';
 
-export function bodyValidator(
+export function requestValidator(
+  property: request,
   schema: Joi.ObjectSchema<any> | Joi.ArraySchema<any[]>
 ): (req: Request, _res: Response, next: NextFunction) => void {
   return (req: Request, _res: Response, next: NextFunction) => {
-    const body = req.body;
+    const body = req[property];
     const { error } = schema.validate(body, { abortEarly: false });
-    if (error) {
-      next(boom.badData('Bad Data', error));
-    }
-    next();
-  };
-}
-
-export function paramsValidator(
-  schema: Joi.ObjectSchema<any>
-): (req: Request, _res: Response, next: NextFunction) => void {
-  return (req: Request, _res: Response, next: NextFunction) => {
-    const params = req.params;
-    const { error } = schema.validate(params, { abortEarly: false });
     if (error) {
       next(boom.badData('Bad Data', error));
     }
