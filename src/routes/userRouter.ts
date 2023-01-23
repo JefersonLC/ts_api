9a0942bf54d84e74c8fe78c97ad846e059d028ef
JefersonLC <c.jeferson.lc@gmail.com>
2023-01-22 21:59:22 -0500

    updated userRouter.ts

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -13,29 +13,30 @@ import {
   getUserSchema,
   updateUserSchema,
 } from '../db/schemas/userSchema';
-import { bodyValidator, paramsValidator } from '../middlewares/validator';
+import { requestValidator } from '../middlewares/validator';
 import passport from 'passport';
 import { checkRole } from '../middlewares/auth';
+import { request } from '../types/requestEnum';
 
 export const userRouter: Router = Router();
 
 userRouter
   .route('/')
   .get(passport.authenticate('jwt', { session: false }), checkRole, getUsers)
-  .post(bodyValidator(createUserSchema), createUser);
+  .post(requestValidator(request.body, createUserSchema), createUser);
 
 userRouter
   .route('/id/:id')
   .all(passport.authenticate('jwt', { session: false }))
   .get(checkRole, getUserById)
-  .all(paramsValidator(getUserSchema))
-  .patch(bodyValidator(updateUserSchema), updateUser)
+  .all(requestValidator(request.params, getUserSchema))
+  .patch(requestValidator(request.body, updateUserSchema), updateUser)
   .delete(deleteUser);
 
 userRouter.get(
   '/role/:role',
   passport.authenticate('jwt', { session: false }),
   checkRole,
-  paramsValidator(getUserByRoleSchema),
+  requestValidator(request.params, getUserByRoleSchema),
   getUsersByRole
 );
