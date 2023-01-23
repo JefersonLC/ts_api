504d8903e5bcc68a8c9207bf0d6f23a7aefda07d
JefersonLC <c.jeferson.lc@gmail.com>
2023-01-22 21:58:32 -0500

    updated index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,18 +5,45 @@ import { categoryRouter } from './categoryRouter';
 import { productRouter } from './productRouter';
 import { orderRouter } from './orderRouter';
 import { authUser } from '../controllers/UserController';
-import { bodyValidator } from '../middlewares/validator';
+import { requestValidator } from '../middlewares/validator';
 import { logInUserSchema } from '../db/schemas/userSchema';
+import { request } from '../types/requestEnum';
+
+// import multer from 'multer';
+// import fs from 'fs';
+// import path from 'path'
 
 export const apiRouter: Router = Router();
 
 apiRouter.post(
   '/login',
-  bodyValidator(logInUserSchema),
+  requestValidator(request.body, logInUserSchema),
   passport.authenticate('local', { session: false }),
   authUser
 );
+apiRouter.get('/verify', authUser);
 apiRouter.use('/users', userRouter);
 apiRouter.use('/categories', categoryRouter);
 apiRouter.use('/products', productRouter);
 apiRouter.use('/orders', orderRouter);
+
+// const storage = multer.diskStorage({
+//   destination: function (_req, _file, cb) {
+//     const root = path.resolve(__dirname, '../..')
+//     const dir = path.join(root, '/public/images/products');
+//     if (!fs.existsSync(dir)) {
+//       fs.mkdirSync(dir);
+//     }
+//     cb(null, dir);
+//   },
+//   filename: function (_req, file, cb) {
+//     cb(null, Date.now() + '-' + file.originalname);
+//   },
+// });
+
+// const upload = multer({ storage: storage });
+
+// apiRouter.post('/img', upload.single('image'), (req, res) => {
+//   const body = req.body;
+//   res.json(body);
+// });
