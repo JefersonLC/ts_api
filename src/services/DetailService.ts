6145f331fb730ed9778b5336abdd3486554f04fa
JefersonLC <c.jeferson.lc@gmail.com>
2023-01-23 19:11:20 -0500

    deleted comments

--- src/services/DetailService.ts
+++ src/services/DetailService.ts
@@ -1,4 +1,3 @@
-// import boom from '@hapi/boom';
 import ShortUniqueId from 'short-unique-id';
 import { InsertResult } from 'typeorm';
 import { AppDataSource } from '../db';
@@ -11,20 +10,6 @@ const uid: ShortUniqueId = new ShortUniqueId({ length: 10 });
 const productService = new ProductService();
 
 export default class DetailService {
-  // async findById(id: string): Promise<Detail> {
-  //   const detail: Detail | null = await AppDataSource.getRepository(
-  //     Detail
-  //   ).findOne({
-  //     where: {
-  //       id: id,
-  //     },
-  //   });
-  //   if (!detail) {
-  //     throw boom.notFound('Detail not found');
-  //   }
-  //   return detail;
-  // }
-
   async createDetail(data: NewOrder, order: Order) {
     const response = data.details.map(async (detail) => {
       const product = await productService.findByIdOrDeleteOrder(
@@ -50,12 +35,4 @@ export default class DetailService {
       .execute();
     return result;
   }
-
-  // async deleteDetail(id: string) {
-  //   const detail = await this.findById(id);
-  //   const result: Detail = await AppDataSource.getRepository(Detail).remove(
-  //     detail
-  //   );
-  //   return result;
-  // }
 }
