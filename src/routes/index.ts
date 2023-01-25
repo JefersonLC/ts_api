da488359547a44982aae0bc1cdf8b9136416037f
JefersonLC <c.jeferson.lc@gmail.com>
2023-01-24 21:25:16 -0500

    test multer

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,9 +9,10 @@ import { requestValidator } from '../middlewares/validator';
 import { logInUserSchema } from '../db/schemas/userSchema';
 import { request } from '../types/requestEnum';
 
-// import multer from 'multer';
-// import fs from 'fs';
-// import path from 'path'
+import multer from 'multer';
+import fs from 'fs';
+import path from 'path'
+import boom from '@hapi/boom';
 
 export const apiRouter: Router = Router();
 
@@ -27,23 +28,30 @@ apiRouter.use('/categories', categoryRouter);
 apiRouter.use('/products', productRouter);
 apiRouter.use('/orders', orderRouter);
 
-// const storage = multer.diskStorage({
-//   destination: function (_req, _file, cb) {
-//     const root = path.resolve(__dirname, '../..')
-//     const dir = path.join(root, '/public/images/products');
-//     if (!fs.existsSync(dir)) {
-//       fs.mkdirSync(dir);
-//     }
-//     cb(null, dir);
-//   },
-//   filename: function (_req, file, cb) {
-//     cb(null, Date.now() + '-' + file.originalname);
-//   },
-// });
 
-// const upload = multer({ storage: storage });
+const storage = multer.diskStorage({
+  destination: function (_req, _file, cb) {
+    const root = path.resolve(__dirname, '../..')
+    const dir = path.join(root, '/public/images/products');
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+    cb(null, dir);
+  },
+  filename: function (_req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
 
-// apiRouter.post('/img', upload.single('image'), (req, res) => {
-//   const body = req.body;
-//   res.json(body);
-// });
+const upload = multer({ storage: storage });
+
+apiRouter.post('/img', 
+upload.single('image'), (req, res) => {
+  console.log(req.body,req.file);
+  if(!req.file) {
+    throw boom.badRequest('se esperaba una imagen')
+  }
+  res.json({
+    res: 'success'
+  });
+});
