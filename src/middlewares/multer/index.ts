281948046c0d79154804a836e2c4d1c413b3fa37
JefersonLC <c.jeferson.lc@gmail.com>
2023-01-25 19:07:02 -0500

    deploy

--- src/middlewares/multer/index.ts
+++ src/middlewares/multer/index.ts
@@ -1,19 +1,19 @@
-import multer from 'multer';
+import * as multer from 'multer';
 import fs from 'fs';
 import path from 'path';
 
 const storage = multer.diskStorage({
-  destination: function (_req, _file, cb) {
-    const root = path.resolve(__dirname, '../../..');
-    const dir = path.join(root, '/public/images/products');
+  destination: function (_req, _file, cb): void {
+    const root: string = path.resolve(__dirname, '../../..');
+    const dir: string = path.join(root, '/public/images/products');
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir);
     }
     cb(null, dir);
   },
-  filename: function (_req, file, cb) {
+  filename: function (_req, file, cb): void {
     cb(null, Date.now() + '-' + file.originalname);
   },
 });
 
-export const upload = multer({ storage: storage });
+export const upload: multer.Multer = multer.default({ storage: storage });
